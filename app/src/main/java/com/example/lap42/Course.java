2e80748d6365997cd474a0e8a8f4d1e1f57a5ad6
PSaiyanS <92966090+PSaiyanS@users.noreply.github.com>
2023-12-26 19:46:21 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lap42/Course.java
@@ -0,0 +1,17 @@
+package com.example.lap42;
+
+public class Course {
+    public String courseCode;
+    public String courseName;
+    public String lecturerName;
+    public int courseImage;
+
+    public Course(String courseCode, String courseName, String lecturerName, int courseImage) {
+        this.courseCode = courseCode;
+        this.courseName = courseName;
+        this.lecturerName = lecturerName;
+        this.courseImage = courseImage;
+    }
+
+
+}
